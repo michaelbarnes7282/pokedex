ce3254cf9fc34c12302acd63fc34682a63fa33a8
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-04-22 21:35:37 -0700

    made some fixes for mobile views

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -8,7 +8,7 @@ const PokeCard = props => {
     return(
         <div className='pokemon-card'>
             <img src= {require(`../sprites/${props.pokemon.entry_number}.png`)} alt=' a pokemon'/>
-            <h3 className='mx-2' style={{textTransform: 'capitalize'}}>Name: {name}</h3>
+            <h3 className='mx-1' style={{textTransform: 'capitalize'}}>Name: {name}</h3>
             <br/>
             <button className='mx-3' onClick={() => push(`/pokemon/${props.pokemon.entry_number}`)}>More Info</button>
         </div>
